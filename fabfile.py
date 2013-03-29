9dbbc8253c8fafc4fa7d9bc560f7ed0f71f884f1
Federico Mendez <mendezfederico87@gmail.com>
2013-03-29 12:50:16 +0100

    added fab function to upgrade apps

--- fabfile.py
+++ fabfile.py
@@ -3,3 +3,7 @@ from fabric.api import local
 def test_project():
     '''Run tests with correct configuration file.'''
     local('python manage.py test --settings=config.settings.testing')
+
+def upgrade_app(app_name):
+    '''Upgrade given app. Eg: fab upgrade_app:Django '''
+    local('pip install {0} --upgrade'.format(app_name))
